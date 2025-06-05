f0243f04724ac9d7b7b2101f5f2ee749c3b8081a
Evan Parker <eparker@ucar.edu>
2025-06-05 07:26:11 +0000

    Broken test run implementation

--- ci_action/implementation.py
+++ ci_action/implementation.py
@@ -114,6 +114,7 @@ def get_environment_config():
     }
     return config
 
+
 def prepare_and_launch_ci_test(environment_config, ci_config, bundle_repo_path, target_repo_path):
     """The main function that will be called to prepare and launch the CI test.
     
@@ -188,129 +189,38 @@ def prepare_and_launch_ci_test(environment_config, ci_config, bundle_repo_path, 
     s3_path = upload_to_aws(BUILD_CACHE_BUCKET, s3_client, bundle_tarball, s3_file)
 
     # Launch the test
+    if test_select == 'random':
+        chosen_build_environments = [random.choice(BUILD_ENVIRONMENTS)]
+    elif test_select == 'all':
+        chosen_build_environments = [e for e in BUILD_ENVIRONMENTS]
+    else:
+        chosen_build_environments = [test_select]
 
     # Write test lock file
+    # TODO: this will not be included in first pass.
 
     # check the lock file and cancel old jobs for PR
     # TODO: this will not be included in first pass.
 
     # write the test github check runs to the PR.
+     for build_environment in chosen_build_environments:
+        checkrun_id_map = github_client.create_check_runs(
+            build_environment,
+            environment_config['repo_name'],
+            environment_config['owner'],
+            environment_config['trigger_commit'],
+            test_annotations.next_ci_suffix)
 
-    # close as success.
-
-
-
-
-
-def process_event(event_config, ci_config):
-    """Create file for S3 bucket"""
-    timer = TimeCheckpointer()
-    trigger_repo = event_config['repository']
-    trigger_repo_uri = event_config['clone_url']
-    owner = event_config['owner']
-    repo_name = event_config['repo_name']
-    pull_request_number = event_config['pull_request_number']
-    trigger_commit = event_config['trigger_commit']
-
-    # Set up clients.
-    # This is a constructor for the configuration needed to submit AWS Batch jobs.
-    batch_submit_env = aws_client.BatchSubmitConfigFromEnv(timeout=60*240)
-    # S3 client is currently not used.
-    #s3_client = boto3.client('s3')
-
-    # vars that must be pulled from the event.
-    branch_name = ''
-    pull_request_number = -1
-    pr_payload = None
-
-
-    if pull_request_number <= 0 or not trigger_commit:
-        print(f'Found no associated PR or trigger commit')
-        print(f'{event_config}')
-        raise ValueError('bad config, check logs.')
-
-    # Get PRs has been deleted
-    #build_info_payload, run_tests, test_select, build_env_suffix = pr_resolve.get_prs(
-    #    trigger_repo=repo_name,
-    #    trigger_uri=trigger_repo_uri,
-    #    trigger_pr_id=pull_request_number,
-    #    trigger_commit=trigger_commit,
-    #    pr_payload=pr_payload)
-    if not run_tests:
-        print(f'Skipping tests for "{repo_name}#{pull_request_number}"')
-        return
-    test_script = build_info_payload['test_script']
-    short_commit = trigger_commit[0:7]
-    debug_time = build_info_payload['debug_time']
-    print(f'Got PR payload. {timer.checkpoint()}')
-
-    # Map of futures to environment names. Note that future objects are hashable
-    # even before they return, so they can be used as dict keys.
-    check_run_future_to_env = {}
-
-    # Create the check "unit" and "integration" payloads using a thread pool;
-    # The GitHub API is a bit slow and we can take advantage of this by creating
-    # all the check runs in a thread pool allowing each thread to wait on the
-    # requests independently. Each future returns a dictionary with the Check
-    # Run ID mapped to the run type, ex {'unit': -id-, 'integration': -id'}.
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-
-        if test_select == 'random':
-            chosen_build_environments = [random.choice(BUILD_ENVIRONMENTS)]
-        elif test_select == 'all':
-            chosen_build_environments = [e for e in BUILD_ENVIRONMENTS]
-        else:
-            chosen_build_environments = [test_select]
-
-        # Submit check-run create requests via the thread pool (see above).
-        for build_environment in chosen_build_environments:
-            check_run_future = executor.submit(
-                github_client.create_check_runs,
-                build_environment,
-                repo_name,
-                owner,
-                trigger_commit,
-                build_env_suffix)
-            check_run_future_to_env[check_run_future] = build_environment
-
-        # Watch the thread pool futures and create the build jobs when done.
-        for future in concurrent.futures.as_completed(check_run_future_to_env):
-            build_environment = check_run_future_to_env[future]
-            try:
-                check_run_id_map = future.result()
-            except Exception as exc:
-                print(f'Failed to create check runs for {build_environment}'
-                      f'due to exception: {exc}')
-                continue
-
-            # Attach check-run IDs to the PR payload allowing the test runner to
-            # update each check run with a link to the job logs.
-            build_info_payload['check_runs'] = check_run_id_map
-            print(f'Created check runs. {timer.checkpoint()}')
-
-            # The build info message is converted to json, compressed, then
-            # encoded as base64 so that it can be passed to the build container.
-            # This compression is necessary because there is a limit of 8192
-            # characters in the 'overrides' message used by the underlying
-            # ECS run task API.
-            # https://docs.aws.amazon.com/AmazonECS/latest/APIReference/API_RunTask.html
-            build_info_zipped = gzip.compress(json.dumps(build_info_payload).encode('utf8'))
-            build_info_b64 = base64.b64encode(build_info_zipped).decode()
-
-            # Create the AWS Batch job that will execute both the unit and the
-            # integration test. The batch job executes a job definition stored
-            # in AWS (see cfn/batch-backend.yaml for our job definitions). The
-            # job takes the build info json file as an argument and this file
-            # configures the build.
-            job = aws_client.submit_test_batch_job(
-                config=batch_submit_env.get_config(build_environment + build_env_suffix),
-                repo_name=repo_name,
-                commit=short_commit,
-                pr=pull_request_number,
-                test_script=test_script,
-                build_identity=f'{repo_name}-{pull_request_number}-{short_commit}-{build_environment}',
-                debug_time=debug_time,
-                build_info=build_info_b64,
-            )
-            job_arn = job['jobArn']
-            print(f'Submitted Batch Job: "{job_arn}". {timer.checkpoint()}')
+        # Note checkrun_id_map is dict {'unit': unit_run.id, 'integration': integration_run.id}
+        job = aws_client.submit_test_batch_job(
+            config=batch_submit_env.get_config(build_environment + build_env_suffix),
+            repo_name=repo_name,
+            commit=short_commit,
+            pr=pull_request_number,
+            test_script=test_script,
+            build_identity=f'{repo_name}-{pull_request_number}-{short_commit}-{build_environment}',
+            debug_time=debug_time,
+            build_info=build_info_b64,
+        )
+        job_arn = job['jobArn']
+        LOG.info(f'Submitted Batch Job: "{job_arn}". {timer.checkpoint()}')
