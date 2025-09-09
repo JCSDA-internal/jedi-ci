bebacc078fd4d1fd33feac714a2cfcedff8d5fdd
Evan Parker <eparker@ucar.edu>
2025-09-09 15:42:28 -0600

    [bugfix] Remove references to build json - fixes oasim tests. (#25)
    This fixes a unittest dependencies glitch that is currently blocking oasim tests from running. In the prior "CI" codebase, unittest dependencies were being passed in by the json config file. Now they are a parameter set by the AWS Batch caller.
    
    Fixes: JCSDA-internal/oasim#8

--- ci_action/implementation.py
+++ ci_action/implementation.py
@@ -279,7 +279,7 @@ def prepare_and_launch_ci_test(
             trigger_pr=str(config['pull_request_number']),
             integration_run_id=checkrun_id_map['integration'],
             unit_run_id=checkrun_id_map['unit'],
-            unittest_dependencies=' '.join(config['unittest_dependencies']),
+            unittest_dependencies=' '.join(enabled_bundles),
             test_script=config['test_script'],
         )
         job_arn = job['jobArn']
