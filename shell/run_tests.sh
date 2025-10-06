e4ce2013d99b3536764b1c1f48ee76e57f343151
Evan Parker <eparker@ucar.edu>
2025-10-06 15:13:26 -0600

    Revert "short timeout (#38)"
    This reverts commit 8b731e45c75d6da7b5f4f37c079f59cad86e4eaa.

--- shell/run_tests.sh
+++ shell/run_tests.sh
@@ -346,12 +346,7 @@ TEST_TAG=$(head -1 "${BUILD_DIR}/Testing/TAG")
 util.check_run_start_test $TRIGGER_REPO_FULL $SECOND_CHECK_RUN_ID
 
 # Run integration tests.
-TEST_TIMEOUT=180
-# TODO(https://github.com/JCSDA-internal/jedi-ci/issues/37): revert this once we have a permanent fix.
-if [ "${TRIGGER_REPO}" = "soca" ]; then
-    TEST_TIMEOUT=30
-fi
-ctest -LE "${UNITTEST_TAG}|gsibec|rttov|oasim|ropp-ufo" --timeout $TEST_TIMEOUT -C RelWithDebInfo -D ExperimentalTest
+ctest -LE "${UNITTEST_TAG}|gsibec|rttov|oasim|ropp-ufo" --timeout 180 -C RelWithDebInfo -D ExperimentalTest
 
 # Upload ctests.
 ctest -C RelWithDebInfo -D ExperimentalSubmit -M Continuous -- --track Continuous --group Continuous
