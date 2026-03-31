eb4a8bf0e40aa745f3378c5522a8be315c269399
Evan Parker <eparker@ucar.edu>
2026-03-31 15:15:34 -0600

    ctest flags updates

--- shell/run_tests.sh
+++ shell/run_tests.sh
@@ -68,6 +68,11 @@ if [ -z "${TRIGGER_REPO_FULL}" ]; then
     echo "Var TRIGGER_REPO_FULL must be set."
     valid_environment_found="no"
 fi
+if [ -z "${CDASH_TEST_MODE}" ]; then
+    # This variable is set by AWS Batch.
+    echo "Var CDASH_TEST_MODE must be set; allowed values include Continuous and Nightly."
+    valid_environment_found="no"
+fi
 
 
 if [ $valid_environment_found == "no" ]; then
@@ -89,6 +94,7 @@ OMPI_ALLOW_RUN_AS_ROOT=${OMPI_ALLOW_RUN_AS_ROOT}
 OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=${OMPI_ALLOW_RUN_AS_ROOT_CONFIRM}
 OMPI_MCA_rmaps_base_oversubscribe=${OMPI_MCA_rmaps_base_oversubscribe}
 CI_SCRIPTS_DIR=${CI_SCRIPTS_DIR}
+CDASH_TEST_MODE=${CDASH_TEST_MODE}
 CC="${CC}"
 CXX="${CXX}"
 FC="${FC}"
@@ -264,13 +270,13 @@ util.check_run_start_test $TRIGGER_REPO_FULL $FIRST_CHECK_RUN_ID
 
 # Run unit tests.
 if [ "${UNIT_RUN_ID}" -eq 0 ]; then
-    ctest $(util.ctest_LE_flag "${ENV_CTEST_EXCLUDES}") --timeout 500 -C RelWithDebInfo -D ExperimentalTest
+    ctest $(util.ctest_LE_flag "${ENV_CTEST_EXCLUDES}") --timeout 500 -C RelWithDebInfo -M "${CDASH_TEST_MODE}" -T Test
 else
-    ctest $(util.ctest_LE_flag "${ENV_CTEST_EXCLUDES}") -L "${UNITTEST_TAG}" --timeout 500 -C RelWithDebInfo -D ExperimentalTest
+    ctest $(util.ctest_LE_flag "${ENV_CTEST_EXCLUDES}") -L "${UNITTEST_TAG}" --timeout 500 -C RelWithDebInfo -M "${CDASH_TEST_MODE}" -T Test
 fi
 
 # Upload ctests.
-ctest -C RelWithDebInfo -D ExperimentalSubmit -M Continuous --track Continuous --group Continuous
+ctest -C RelWithDebInfo -M "${CDASH_TEST_MODE}" -T Submit
 
 echo "CDash URL: $(util.create_cdash_url "${BUILD_DIR}/Testing")"
 
@@ -357,10 +363,10 @@ TEST_TAG=$(head -1 "${BUILD_DIR}/Testing/TAG")
 util.check_run_start_test $TRIGGER_REPO_FULL $SECOND_CHECK_RUN_ID
 
 # Run integration tests.
-ctest $(util.ctest_LE_flag "${ENV_CTEST_EXCLUDES}|${UNITTEST_TAG}|tier2|gsibec|rttov|oasim|ropp-ufo") --timeout 180 -C RelWithDebInfo -D ExperimentalTest
+ctest $(util.ctest_LE_flag "${ENV_CTEST_EXCLUDES}|${UNITTEST_TAG}|tier2|gsibec|rttov|oasim|ropp-ufo") --timeout 180 -C RelWithDebInfo -M "${CDASH_TEST_MODE}" -T Test
 
 # Upload ctests.
-ctest -C RelWithDebInfo -D ExperimentalSubmit -M Continuous --track Continuous --group Continuous
+ctest -C RelWithDebInfo -M "${CDASH_TEST_MODE}" -T Submit
 
 find ${BUILD_DIR}/Testing -type f
 find ${BUILD_DIR}/Testing -type f -exec head -n5 {} \;
