7b8c85d132039e81502bc69f5dc6524126fe5516
Evan Parker <eparker@ucar.edu>
2026-06-10 14:12:25 -0600

    add "SCCACHE_SERVER_UDS" config.

--- shell/run_tests.sh
+++ shell/run_tests.sh
@@ -125,6 +125,7 @@ set -x
 #
 # Setup sccache for caching. This will be handled by the image in the future but temporarily do setup here.
 #
+
 if [[ ! $(which sccache) ]]; then
     wget --no-verbose https://github.com/mozilla/sccache/releases/download/v0.14.0/sccache-v0.14.0-x86_64-unknown-linux-musl.tar.gz
     tar -xvf sccache-v0.14.0-x86_64-unknown-linux-musl.tar.gz
@@ -133,6 +134,7 @@ if [[ ! $(which sccache) ]]; then
 fi
 
 # Export sccache AWS bucket config.
+export SCCACHE_SERVER_UDS="${WORKDIR}/sccache.sock"
 export SCCACHE_BUCKET="${CACHE_BUCKET}"
 export SCCACHE_REGION="us-east-2"
 export SCCACHE_S3_KEY_PREFIX="sccache-$JEDI_COMPILER-$($CC -dumpversion | tr -d '.')"
